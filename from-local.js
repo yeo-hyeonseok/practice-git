6c6f1b15e485a8e63108c782bf1da5286f0145dc
GeniuStone <dugustlr@naver.com>
2023-01-16 18:20:13 +0900

    from-local.js 파일 생성

--- from-local.js
+++ from-local.js
@@ -1,1 +1,11 @@
 console.log("로컬저장소에서 생성한 파일이에요");
+// clone 해왔던 기존 원격 저장소에 다른 사람이 push한 내용이 있다면 나도 연달아 push 불가능, 먼저 pull 하세요~
+// 원격저장소의 최신내용이 내 로컬저장소에 있을 때만 push가 가능
+
+/*
+error: failed to push some refs to 'https://github.com/yeo-hyeonseok/practice-git.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details. */
